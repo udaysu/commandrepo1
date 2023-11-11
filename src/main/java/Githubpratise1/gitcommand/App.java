c2fe705a3361521df41a013294db765f6910cb62
udaysu <suryauday90@gmail.com>
2023-11-11 14:31:57 +0530

    first commit

---
+++ src/main/java/Githubpratise1/gitcommand/App.java
@@ -0,0 +1,13 @@
+package Githubpratise1.gitcommand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
